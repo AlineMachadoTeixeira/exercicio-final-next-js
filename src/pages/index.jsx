52b8ed3ee60a38102c65703eb3966871f2823804
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-02-01 09:52:41 -0300

    css do H3 e li

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,25 @@ export default function Home() {
         <meta name="Keywords" content="Frutas, melancia, feira, banana, maça" />
       </Head>
       <section>
-        <h2>Frutado</h2>
+        <h3>O que elas fazem por você?</h3>
+        <ul>
+          <li>
+            <strong>Nutrientes Essenciais:</strong> Vitaminas e minerais
+            fortalecem o sistema imunológico e promovem saúde.
+          </li>
+          <li>
+            <strong>Antioxidantes Protetores:</strong>Combatem radicais livres,
+            reduzindo o envelhecimento precoce e risco de doenças.
+          </li>
+          <li>
+            <strong>Energia Sustentável:</strong>Açúcares naturais fornecem
+            energia estável ao longo do dia.
+          </li>
+          <li>
+            <strong>Hidratação Natural:</strong>Alto teor de água contribui para
+            a hidratação do corpo.
+          </li>
+        </ul>
       </section>
     </>
   );
