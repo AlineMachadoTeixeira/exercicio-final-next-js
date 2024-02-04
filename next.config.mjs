bba47342a801fc608fe9bc30e013f3a687bb50db
Aline Teixeira <alinemachadoteixeira@gmail.com>
2024-02-04 19:04:58 -0300

    next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
 };
+module.exports = withPWA(nextConfig);
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+// };
+
+// export default nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,22 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
-        <link rel="shortcut icon" href="/images/frutado-logo.svg"></link>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="application-name" content="PetShop" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PetShop" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link
+          rel="shortcut icon"
+          href="/images/frutado-logo.svg"
+          type="image/png"
+          sizes="256x256"
+        />
+
+        {/* <link rel="shortcut icon" href="/images/frutado-logo.svg"></link> */}
+
+        <meta name="author" content="Aline Teixeira" />
       </Head>
       <body>
         <Main />
